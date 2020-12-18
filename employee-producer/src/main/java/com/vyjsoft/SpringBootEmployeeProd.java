2883084cd5a3c60c47116d97754b881add1c4bb8
Jose Gomez <josegelimergomez@gmail.com>
2020-12-18 10:06:03 -0300

    first commit

---
+++ employee-producer/src/main/java/com/vyjsoft/SpringBootEmployeeProd.java
@@ -0,0 +1,16 @@
+package com.vyjsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootEmployeeProd {
+
+	public static void main(String[] args) {
+		 
+		SpringApplication.run(SpringBootEmployeeProd.class, args);
+
+	}
+
+}
---
+++ employee-producer/src/main/java/com/vyjsoft/model/Employee.java
@@ -0,0 +1,66 @@
+package com.vyjsoft.model;
+
+public class Employee {
+
+	private String empID;
+	private String name;
+	private String designation;
+	private double salary;
+	
+	
+	
+	public Employee() {
+		super();
+	}
+
+
+
+	public String getEmpID() {
+		return empID;
+	}
+
+
+
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDesignation() {
+		return designation;
+	}
+
+
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
